420d67bf450d9bf0a7e5a3ceeb4b176baf7df05e
AbrahamOlsen <cabrahamolsenchaves8@gmail.com>
2024-02-19 22:25:04 -0600

    Fix: Layout fixed

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import "./App.css";
 import { useEffect, useState } from "react";
-import Poke from "./Poke"
+import Poke from "./Poke";
 function App() {
   const [poke, setPoke] = useState([]);
   const [pokemonName, setPokemonName] = useState([]);
@@ -8,7 +8,7 @@ function App() {
   const [img, setImg] = useState(0);
   //const id=0, name="", img="";
   const url = "https://pokeapi.co/api/v2/pokemon?limit=150&offset=0";
-  const search_url = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`
+  const search_url = `https://pokeapi.co/api/v2/pokemon/${pokemonName}`;
 
   useEffect(() => {
     const getPoke = async () => {
@@ -31,56 +31,62 @@ function App() {
     getPoke();
   }, []);
 
-  
-
-
   const getPokemonByName = async () => {
     const resp = await fetch(search_url);
     const data = await resp.json();
     const img = data.sprites.front_default;
-    console.log(img)
-    setImg(img)
-    setSearched(data)
-  }
+    console.log(img);
+    setImg(img);
+    setSearched(data);
+  };
 
-
-  
-
-  const onChange = (e)=>{ 
-    setPokemonName(e.target.value)
-    console.log(pokemonName)
-  }
-  const onSubmit = (e)=>{
-    e.preventDefault()
-    console.log(pokemonName)
-    setPokemonName("")
-    getPokemonByName()
-  }
-  
+  const onChange = (e) => {
+    setPokemonName(e.target.value);
+    console.log(pokemonName);
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log(pokemonName);
+    setPokemonName("");
+    getPokemonByName();
+  };
 
   return (
+    <>
+    <div className="imagen">
+    <img  src="../src/assets/pokemon.png"></img>
+    </div>
     <div className="main">
-      {/* <img src="assets/pokemon.png" width="200px" /> */}
+    
 
-      
       <div className="container-app">
-
         {poke.map((poke) => (
-
-          <Poke key={poke.id} id={poke.id} name={poke.name} img={poke.img} ></Poke>
-
+          <Poke
+            key={poke.id}
+            id={poke.id}
+            name={poke.name}
+            img={poke.img}
+          ></Poke>
         ))}
-
       </div>
       <div className="search-box">
-        <form onSubmit={(event)=>onSubmit(event)}>
-              <input value={pokemonName} type="text" placeholder="Search Pokemon" onChange={ (value)=>onChange(value) }/>
+        <form onSubmit={(event) => onSubmit(event)}>
+          <input
+            value={pokemonName}
+            type="text"
+            placeholder="Search Pokemon"
+            onChange={(value) => onChange(value)}
+          />
         </form>
-        <Poke key={searched.id} id={searched.id} name={searched.name} img={img} ></Poke>
-        </div>
-
-
+        <Poke
+          key={searched.id}
+          id={searched.id}
+          name={searched.name}
+          img={img}
+        ></Poke>
+      </div>
     </div>
+    </>
   );
 }
 
--- src/Poke.jsx
+++ src/Poke.jsx
@@ -6,7 +6,7 @@ const Pokemon = (props) => {
       <h2 className="name">{props.name}</h2>
 
         <div key={props.id} className="card">
-          <img className="img" src={props.img}></img>
+          <img className="img" alt="Search a Pokemon!!" src={props.img}></img>
           {/* <p>{props.id}</p> */}
         </div>
 
